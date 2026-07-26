4763c659de07d9182df90a52c4289adac75b41a3
Erwin Asseng <erwinalberic9@gmail.com>
2026-07-26 19:51:02 +0100

    mise a jour supabase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseKey: 'sb_publishable_gYfeXid0dBInl18m4W7NPw_I4pcxWU0'
+  supabaseUrl: 'https://xvgknkxigkktrcdlejbc.supabase.co',
+  supabaseKey: 'sb_publishable_0QQNUY65AkUfJ-A_bACv5g_K6wa7VwS'
 };
